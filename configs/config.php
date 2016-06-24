1fe1f2a77f8738cb7b5a8d73f64f03aa0f83d231
Scorpion <scorpionukr@users.noreply.github.com>
2016-06-24 19:37:39 +0300

    Update config.php

--- configs/config.php
+++ configs/config.php
@@ -10,14 +10,14 @@ $curTime=date("H:i:s");
 // MySQL servers
 // connect to CRM MySQL
 $host="localhost";
-$user="scorpion";
-$password="fyybubkzwbz";
+$user="";
+$password="";
 $dbName="crm";
 $dbc = mysqli_connect($host, $user, $password, $dbName) or die("Error connect CRM MySQL server: " . mysqli_error($dbc)); 
 // connect to Asterisk MySQL
 $hostA="localhost";
-$userA="scorpion";
-$passwordA="fyybubkzwbz";
+$userA="";
+$passwordA="";
 $dbNameA="asteriskcdrdb";
 $dbca = mysqli_connect($hostA, $userA, $passwordA, $dbNameA) or die("Error connect Asterisk MySQL server: " . mysqli_error($dbca)); 
 // Автор
